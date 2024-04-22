8a6c0a40ecfe7b88f9829e2c5d2fd801afe8bf86
DarylMartinDipp <didieravangers@gmail.com>
2024-04-22 15:44:55 +0200

    Application and Controller documentation.

--- src/main/java/com/dauphine/blogger/controllers/HelloWorldController.java
+++ src/main/java/com/dauphine/blogger/controllers/HelloWorldController.java
@@ -1,8 +1,13 @@
 package com.dauphine.blogger.controllers;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Tag(
+        name = "Hello World! API",
+        description = "My first Hello World! endpoints."
+)
 public class HelloWorldController {
     @GetMapping("hello_world")
     public String helloWorld() {
@@ -16,6 +21,6 @@ public class HelloWorldController {
 
     @GetMapping("hello/{name}")
     public String hello(@PathVariable String name) {
-        return "Welcome " + name + "!";
+        return "Hello " + name + "!";
     }
 }
