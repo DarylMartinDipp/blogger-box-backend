e607fd990ddc2a64caf8ac7a71b049e601fe6c40
DarylMartinDipp <didieravangers@gmail.com>
2024-04-22 15:50:11 +0200

    Endpoint documentation.

--- src/main/java/com/dauphine/blogger/controllers/HelloWorldController.java
+++ src/main/java/com/dauphine/blogger/controllers/HelloWorldController.java
@@ -1,26 +1,46 @@
 package com.dauphine.blogger.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Tag(
         name = "Hello World! API",
-        description = "My first Hello World! endpoints."
+        description = "My first Hello World! endpoints"
 )
 public class HelloWorldController {
     @GetMapping("hello_world")
+    @Operation(
+            summary = "Hello World! endpoint",
+            description = "Return 'Hello World!'"
+    )
     public String helloWorld() {
         return "Hello World!";
     }
 
     @GetMapping("hello")
-    public String helloByName(@RequestParam String name) {
+    @Operation(
+            summary = "Hello by name in parameters endpoint",
+            description = "Return 'Hello {name}!' by requested parameter"
+    )
+    public String helloByName(
+            @Parameter(description = "Name to greet")
+            @RequestParam String name
+    ) {
         return "Hello " + name + "!";
     }
 
     @GetMapping("hello/{name}")
-    public String hello(@PathVariable String name) {
+    @Operation(
+            summary = "Hello by name endpoint",
+            description = "Return 'Hello {name}!' by path variable"
+    )
+    public String hello(
+            @Parameter(description = "Name to greet")
+            @PathVariable String name
+    ) {
         return "Hello " + name + "!";
     }
 }
