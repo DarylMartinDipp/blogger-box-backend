735db4651be96ab286d210b00404906d2c4d3485
DarylMartinDipp <didieravangers@gmail.com>
2024-04-22 15:00:15 +0200

    HelloWorld and HelloName

--- src/main/java/com/dauphine/blogger/controllers/HelloWorldController.java
+++ src/main/java/com/dauphine/blogger/controllers/HelloWorldController.java
@@ -1,4 +1,18 @@
 package com.dauphine.blogger.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+    @GetMapping("hello_world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+    @GetMapping("hello_name")
+    public String helloByName(@RequestParam String name) {
+        return "Hello " + name + "!";
+    }
 }
