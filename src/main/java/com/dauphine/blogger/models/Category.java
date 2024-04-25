411772bcabe08556949884a0f3ebcd3eb4cf0bf7
DarylMartinDipp <didieravangers@gmail.com>
2024-04-25 16:56:15 +0200

    Connect to database

--- src/main/java/com/dauphine/blogger/models/Category.java
+++ src/main/java/com/dauphine/blogger/models/Category.java
@@ -1,14 +1,29 @@
 package com.dauphine.blogger.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import java.util.UUID;
 
+@Entity
+@Table(name = "category")
 public class Category {
+    @Id
+    @Column(name = "id")
     private UUID id;
+
+    @Column(name = "name")
     private String name;
 
     public Category(UUID id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    //  Very important to have an empty constructor and getters and setters for each property
+    public Category() {
     }
 
     public UUID getId() {
--- src/main/java/com/dauphine/blogger/models/Post.java
+++ src/main/java/com/dauphine/blogger/models/Post.java
@@ -1,4 +1,68 @@
 package com.dauphine.blogger.models;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "post")
 public class Post {
+
+    @Id
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
